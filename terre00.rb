9ded670aaa8049711b6220a967f17ff993705d66
Hugo <h.granier@me.com>
2022-01-19 17:11:48 +0100

    terminer?

--- terre00.rb
+++ terre00.rb
@@ -1,20 +1,10 @@
-=begin
-Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
-
-
-Exemples d’utilisation :
-$> python exo.py
-abcdefghijklmnopqrstuvwxyz
-$>
-
-
-Attention : votre programme devra utiliser une boucle.
-=end
+#Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
 
 def alphabets
 	alphabet_entier = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 		for alphabet in alphabet_entier
-			puts "#{alphabet}"
+			print "#{alphabet}"
 		end
 end
 
+alphabets
